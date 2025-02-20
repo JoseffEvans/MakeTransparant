497e69fd1d2a845f305e138661f51c257347edec
Ducky <Ducky@DESKTOP-UTU3BFP>
2025-02-20 14:31:11 +0000

    Add project files.

---
+++ MakeTransparant/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MakeTransparent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            InputPng = new TextBox();
+            InputPathLabel = new Label();
+            InputPngError = new Label();
+            OutputPngPath = new TextBox();
+            label1 = new Label();
+            OutputPngError = new Label();
+            Execute = new Button();
+            ExecuteError = new Label();
+            Success = new Label();
+            SuspendLayout();
+            // 
+            // InputPng
+            // 
+            InputPng.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            InputPng.BackColor = SystemColors.ControlDark;
+            InputPng.Location = new Point(12, 26);
+            InputPng.Name = "InputPng";
+            InputPng.Size = new Size(776, 23);
+            InputPng.TabIndex = 0;
+            InputPng.TextChanged += InputPng_TextChanged;
+            InputPng.MouseLeave += InputPng_MouseLeave;
+            // 
+            // InputPathLabel
+            // 
+            InputPathLabel.AutoSize = true;
+            InputPathLabel.ForeColor = Color.White;
+            InputPathLabel.Location = new Point(12, 8);
+            InputPathLabel.Name = "InputPathLabel";
+            InputPathLabel.Size = new Size(89, 15);
+            InputPathLabel.TabIndex = 1;
+            InputPathLabel.Text = "Input PNG Path";
+            // 
+            // InputPngError
+            // 
+            InputPngError.AutoSize = true;
+            InputPngError.ForeColor = Color.LightCoral;
+            InputPngError.Location = new Point(107, 8);
+            InputPngError.Name = "InputPngError";
+            InputPngError.Size = new Size(43, 15);
+            InputPngError.TabIndex = 2;
+            InputPngError.Text = "ERROR";
+            InputPngError.Visible = false;
+            // 
+            // OutputPngPath
+            // 
+            OutputPngPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            OutputPngPath.BackColor = SystemColors.ControlDark;
+            OutputPngPath.Location = new Point(12, 77);
+            OutputPngPath.Name = "OutputPngPath";
+            OutputPngPath.Size = new Size(776, 23);
+            OutputPngPath.TabIndex = 3;
+            OutputPngPath.MouseLeave += OutputPngPath_MouseLeave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Output PNG Path";
+            // 
+            // OutputPngError
+            // 
+            OutputPngError.AutoSize = true;
+            OutputPngError.ForeColor = Color.LightCoral;
+            OutputPngError.Location = new Point(117, 59);
+            OutputPngError.Name = "OutputPngError";
+            OutputPngError.Size = new Size(43, 15);
+            OutputPngError.TabIndex = 5;
+            OutputPngError.Text = "ERROR";
+            OutputPngError.Visible = false;
+            // 
+            // Execute
+            // 
+            Execute.Location = new Point(16, 118);
+            Execute.Name = "Execute";
+            Execute.Size = new Size(111, 42);
+            Execute.TabIndex = 6;
+            Execute.Text = "Execute";
+            Execute.UseVisualStyleBackColor = true;
+            Execute.Click += Execute_Click;
+            // 
+            // ExecuteError
+            // 
+            ExecuteError.AutoSize = true;
+            ExecuteError.ForeColor = Color.LightCoral;
+            ExecuteError.Location = new Point(133, 132);
+            ExecuteError.Name = "ExecuteError";
+            ExecuteError.Size = new Size(43, 15);
+            ExecuteError.TabIndex = 7;
+            ExecuteError.Text = "ERROR";
+            ExecuteError.Visible = false;
+            // 
+            // Success
+            // 
+            Success.AutoSize = true;
+            Success.ForeColor = Color.LightGreen;
+            Success.Location = new Point(16, 163);
+            Success.Name = "Success";
+            Success.Size = new Size(48, 15);
+            Success.TabIndex = 8;
+            Success.Text = "Success";
+            Success.Visible = false;
+            // 
+            // Form1
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 238);
+            Controls.Add(Success);
+            Controls.Add(ExecuteError);
+            Controls.Add(Execute);
+            Controls.Add(OutputPngError);
+            Controls.Add(label1);
+            Controls.Add(OutputPngPath);
+            Controls.Add(InputPngError);
+            Controls.Add(InputPathLabel);
+            Controls.Add(InputPng);
+            Name = "Form1";
+            Text = "Make Transparent";
+            DragDrop += Form1_DragDrop;
+            DragEnter += Form1_DragEnter;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox InputPng;
+        private Label InputPathLabel;
+        private Label InputPngError;
+        private TextBox OutputPngPath;
+        private Label label1;
+        private Label OutputPngError;
+        private Button Execute;
+        private Label ExecuteError;
+        private Label Success;
+    }
+}
---
+++ MakeTransparant/Form1.cs
@@ -0,0 +1,153 @@
+using System.Drawing.Imaging;
+
+namespace MakeTransparent {
+    public partial class Form1 : Form {
+
+        readonly Color Transparant = Color.FromArgb(0, 255, 255, 255);
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void InputPng_TextChanged(object sender, EventArgs e) {
+            TrimTextBoxQuotes((TextBox)sender);
+        }
+
+        private void InputPng_MouseLeave(object sender, EventArgs e) {
+            ValidateInputPng();
+        }
+
+        private void OutputPngPath_MouseLeave(object sender, EventArgs e) {
+            ValidateOutputPng();
+        }
+
+        private void Execute_Click(object sender, EventArgs e) {
+            Success.Visible = false;
+            Execute.Enabled = false;
+            var valid = ValidateForExecute();
+            if (valid) ReplacePixels();
+            Execute.Enabled = true;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e) {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (filePaths.Length != 0) {
+                InputPng.Text = filePaths[0];
+                ValidateInputPng();
+            }
+        }
+
+        static void TrimTextBoxQuotes(TextBox textBox) {
+            if (textBox.Text.StartsWith('"') || textBox.Text.EndsWith('"'))
+                textBox.Text = textBox.Text.Trim('"');
+        }
+
+        bool ValidateInputPng() {
+            var validation = ValidatePng(InputPng.Text);
+            if (!validation.Error) {
+                InputPngError.Visible = false;
+                if (string.IsNullOrEmpty(OutputPngPath.Text))
+                    OutputPngPath.Text = Path.Combine(
+                        Path.GetDirectoryName(InputPng.Text),
+                        $"{Path.GetFileNameWithoutExtension(InputPng.Text)}-transparant{Path.GetExtension(InputPng.Text)}"
+                    );
+                return true;
+            } else {
+                InputPngError.Visible = true;
+                InputPngError.Text = validation.ErrorMessage;
+                return false;
+            }
+
+        }
+
+        bool ValidateOutputPng() {
+            var validation = ValidateNewFile(OutputPngPath.Text);
+            if (!validation.Error) {
+                OutputPngError.Visible = false;
+                return true;
+            } else {
+                OutputPngError.Visible = true;
+                OutputPngError.Text = validation.ErrorMessage;
+                return false;
+            }
+        }
+
+        bool ValidateForExecute() {
+            var hasErrors = new bool[] {
+                ValidateInputPng(),
+                ValidateOutputPng()
+            }.Any(result => !result);
+
+            if (hasErrors) {
+                ExecuteError.Visible = true;
+                ExecuteError.Text = "Error: Please fix errors in input fields";
+                return false;
+            } else {
+                ExecuteError.Visible = false;
+                return true;
+            }
+        }
+
+        void ReplacePixels() {
+            try {
+                using var bitmap = new Bitmap(InputPng.Text);
+                for (var y = 0; y < bitmap.Height; y++) for (var x = 0; x < bitmap.Width; x++) {
+                        var pixel = bitmap.GetPixel(x, y);
+                        if (IsWhite(pixel))
+                            bitmap.SetPixel(x, y, Transparant);
+                    }
+                bitmap.Save(OutputPngPath.Text, ImageFormat.Png);
+                Success.Visible = true;
+                InputPng.Text = string.Empty;
+                OutputPngPath.Text = string.Empty;
+            } catch (Exception ex) {
+                ExecuteError.Text = $"An error occurred: ${ex.Message}";
+                ExecuteError.Visible = true;
+            }
+        }
+
+        ValidationResult ValidateNewFile(string path) {
+            if (string.IsNullOrEmpty(path))
+                return new ValidationResult("Path must be set");
+
+            if (!Directory.Exists(Path.GetDirectoryName(path)))
+                return new ValidationResult("Must be in a existing directory");
+
+            if (File.Exists(path))
+                return new ValidationResult("File already exists, change its name");
+
+            return new ValidationResult();
+        }
+
+        ValidationResult ValidatePng(string path) {
+            if (string.IsNullOrEmpty(path))
+                return new ValidationResult("Path must be set");
+
+            if (!File.Exists(path))
+                return new ValidationResult("Path does not exist");
+
+            if (!Path.HasExtension(path) || Path.GetExtension(path) != ".png")
+                return new ValidationResult("File must be a PNG");
+
+            return new ValidationResult();
+        }
+
+        bool IsWhite(Color pixel) =>
+            pixel.R == 255 && pixel.G == 255 && pixel.B == 255;
+
+        class ValidationResult {
+            public bool Error;
+            public string ErrorMessage = string.Empty;
+            public ValidationResult() { }
+            public ValidationResult(string errorMessage) {
+                Error = true;
+                ErrorMessage = errorMessage;
+            }
+        }
+    }
+}
